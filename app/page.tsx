ddb5d2ab41e7ceb215497929e308d72b5d5ba6d5
Victor Olmos <victorolmosmartin@gmail.com>
2025-06-10 19:36:47 +0200

    50

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         </nav>
       </header>
       <main className="flex-1">
-        <section className="h-[70vh] py-12 md:py-24 lg:py-32 xl:py-48">
+        <section className="h-[50vh] py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
